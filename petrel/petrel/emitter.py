118077b80e4b15ca32a6268cce15b1c26bd52a2b
Jeff Kolb <jkolb@gnipcentral.com>
2014-04-29 19:40:14 +0000

    added MyEmitterBase class, which accounts for py files not in the submission dir

--- petrel/petrel/emitter.py
+++ petrel/petrel/emitter.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 import storm
 
@@ -27,11 +28,36 @@ class EmitterBase(object):
         else:
             return None
 
-class Spout(EmitterBase, storm.Spout):
+class MyEmitterBase(object):
+    DEFAULT_PYTHON = 'python%d.%d' % (sys.version_info.major, sys.version_info.minor)
+
+    def __init__(self, script):
+        # if script is imported from elsewhere, put a fresh copy in current working dir
+        # also, if there's a precompiled version that's being run, 
+        # strip off the final 'c' for future use of 'script' string
+        path, basename = os.path.split(os.path.relpath(script))
+        if len(path) != 0:
+            shutil.copy(script.rstrip('c'),os.getcwd())
+        script = '%s.py' % os.path.splitext(basename)[0]
+        self.execution_command = self.DEFAULT_PYTHON
+        self.script = script
+        self._json = {}
+        super(MyEmitterBase, self).__init__()
+    
+    def declareOutputFields(declarer):
+        raise NotImplementedError()
+
+    def getComponentConfiguration(self):
+        if len(self._json):
+            return self._json
+        else:
+            return None
+
+class Spout(MyEmitterBase, storm.Spout):
     pass
 
-class BasicBolt(EmitterBase, storm.BasicBolt):
+class BasicBolt(MyEmitterBase, storm.BasicBolt):
     pass
 
-class Bolt(EmitterBase, storm.Bolt):
+class Bolt(MyEmitterBase, storm.Bolt):
     pass
