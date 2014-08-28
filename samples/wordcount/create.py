4519a2d61bdc17d6660641359c6b02d50dac2e8c
Nick Matheson <nmatheson@twitter.com>
2014-08-28 07:53:45 -0600

    nm: adding code changes required for archive spout

--- samples/wordcount/create.py
+++ samples/wordcount/create.py
@@ -6,6 +6,6 @@ from petrel.emitter import JavaSpout
 
 def create(builder):
     #builder.setSpout("spout", randomsentence.RandomSentenceSpout(), 1)
-    builder.setJavaSpout("spout", JavaSpout("storm.petrel.dummy.ActivityStreamsSampleSpout", ['sentence']), 1)
+    builder.setJavaSpout("spout", JavaSpout("com.gnip.archive_spout.storm.ArchiveSpout", ['archive']), 1)
     builder.setBolt("split", splitsentence.SplitSentenceBolt(), 1).shuffleGrouping("spout")
     builder.setBolt("count", wordcount.WordCountBolt(), 1).fieldsGrouping("split", ["word"])
